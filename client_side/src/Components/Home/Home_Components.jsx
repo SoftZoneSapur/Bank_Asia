490d70f00a694909eb827d654d84ff71d95754a2
HataillaPola <softzonesapur@gmail.com>
2023-04-02 17:02:43 +0600

    d
    d

--- client_side/src/Components/Home/Home_Components.jsx
+++ client_side/src/Components/Home/Home_Components.jsx
@@ -46,7 +46,7 @@ const Home = () => {
                 </MDBCarouselItem>
 
                 
-                </MDBCarousel>
+            </MDBCarousel>
 
             </section>
             {/* end section 1  */}
@@ -59,8 +59,8 @@ const Home = () => {
             {/* section 2 start  */}
             <section className='Section_2'>
                 <div className="Section_2_Left_Side">
-                    <h3>AGENT BANKING</h3>
-                    <p>Agent Banking, a very new but powerful and wide-ranged initiative in the journey of Financial Inclusion in Bangladesh. Bank Asia introduced this venture here in January 2014. Earlier in 2013, Bangladesh Bank accorded Bank Asia to roll out this specialized banking service. In span of a few years, this service spreads in multi-dimensional ways, reduces gap of time and space and becomes attractive to rural mass. The small droplets of deposit contribute in building strong platform of our promising economy, and thus infuses lifeline in our rural economy. Now marginal farmers, small entrepreneurs and general people enjoy loan facilities in easier and faster manner through this platform. To reach Agent Banking services at the doorsteps of rural people Government, non-government and development partners shake hands together.</p>
+                    <h3>এজেন্ট ব্যাংকিং</h3>
+                    <p>এজেন্ট ব্যাংকিং বাংলাদেশের অর্থনৈতিক অন্তর্ভুক্তির অগ্রযাত্রায় একটি নতুন, শক্তিশালী এবং ব্যাপক-ভিত্তিক ব্যাংকিং প্রচেষ্টা। ব্যাংক এশিয়ার উদ্যোগে ২০১৪ সালের জানুয়ারীতে বাংলাদেশে এ সেবার যাত্রা শুরু হয়। এর আগে ২০১৩ সালে বাংলাদেশ ব্যাংক এ বিশেষায়িত ব্যাংকিং সেবা পরিচালনার অনুমতি প্রদান করে। মাত্র ক’বছরের ব্যবধানে এ সুদূর প্রসারী উদ্যোগ নানা দিকে, বিচিত্র আঙ্গিকে শাখা মেলতে শুরু করেছে। সময় এবং দূরত্বের ব্যবধান ঘুচিয়ে এ সেবা গ্রামীন জনগোষ্ঠীর কাছে হৃদয়গ্রাহী হয়ে উঠছে। তাদের ক্ষুদ্র সঞ্চয় ব্যাংকিং চ্যানেলে জমা হয়ে গড়ে উঠছে সম্ভাবনাময় অর্থনীতির এক শক্তিশালী ভিত্তি। প্রাণ সঞ্চারিত হচ্ছে গ্রামীন অর্থনীতিতে। প্রান্তিক কৃষক, ক্ষুদ্র উদ্যোক্তা সহ সকলে এজেন্ট ব্যাংকিং মোবাইল অ্যাপস-এর কল্যানে সহজেই ঋণ সুবিধা পাচ্ছে। জনগণের দোরগোড়ায় দ্রুত এ সেবা পৌঁছে দিতে সরকারি, বেসরকারী বিভিন্ন উন্নয়ন সংস্থা এজেন্ট ব্যাংকিং উদ্যোগের সাথে সম্পৃক্ত হয়েছে।</p>
 
                     <h3>WHERE WE ARE TODAY</h3>
 
@@ -74,7 +74,41 @@ const Home = () => {
 
 
                 <div className="Section_2_Right_Side">
+                    <h3>সর্বশেষ সংবাদ</h3>
+                    <hr />
+                <MDBCarousel showIndicators showControls fade>
+                    <MDBCarouselItem
+                        className='w-100 d-block'
+                        itemId={1}
+                        src='https://www.bankasia-bd.com/agentassets/slider/flash-1.jpg'
+                        alt='...'
+                    >
+                        <h5>First slide label</h5>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </MDBCarouselItem>
 
+                    <MDBCarouselItem
+                        className='w-100 d-block'
+                        itemId={2}
+                        src='https://www.bankasia-bd.com/agentassets/slider/flash-102.jpg'
+                        alt='...'
+                    >
+                        <h5>Second slide label</h5>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </MDBCarouselItem>
+
+                    <MDBCarouselItem
+                        className='w-100 d-block'
+                        itemId={3}
+                        src='https://www.bankasia-bd.com/agentassets/slider/flash-3.jpg'
+                        alt='...'
+                    >
+                        <h5>Third slide label</h5>
+                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    </MDBCarouselItem>
+
+                
+                </MDBCarousel>
                 </div>
 
 
