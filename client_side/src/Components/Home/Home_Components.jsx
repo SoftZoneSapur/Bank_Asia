a31f770edddcd94cdbe0e1f97a99bdb1684ca9ea
HataillaPola <softzonesapur@gmail.com>
2023-04-03 16:07:30 +0600

    Update Home_Components.jsx

--- client_side/src/Components/Home/Home_Components.jsx
+++ client_side/src/Components/Home/Home_Components.jsx
@@ -74,41 +74,41 @@ const Home = () => {
 
 
                 <div className="Section_2_Right_Side">
-                    <h3>সর্বশেষ সংবাদ</h3>
-                    <hr />
-                <MDBCarousel showIndicators showControls fade>
-                    <MDBCarouselItem
-                        className='w-100 d-block'
-                        itemId={1}
-                        src='https://www.bankasia-bd.com/agentassets/slider/flash-1.jpg'
-                        alt='...'
-                    >
-                        <h5>First slide label</h5>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </MDBCarouselItem>
+                        <h3>সর্বশেষ সংবাদ</h3>
+                        <hr />
+                    <MDBCarousel showIndicators showControls fade>
+                        <MDBCarouselItem
+                            className='w-100 d-block'
+                            itemId={1}
+                            src='https://www.bankasia-bd.com/agentassets/slider/flash-1.jpg'
+                            alt='...'
+                        >
+                            <h5>First slide label</h5>
+                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        </MDBCarouselItem>
 
-                    <MDBCarouselItem
-                        className='w-100 d-block'
-                        itemId={2}
-                        src='https://www.bankasia-bd.com/agentassets/slider/flash-102.jpg'
-                        alt='...'
-                    >
-                        <h5>Second slide label</h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </MDBCarouselItem>
+                        <MDBCarouselItem
+                            className='w-100 d-block'
+                            itemId={2}
+                            src='https://www.bankasia-bd.com/agentassets/slider/flash-102.jpg'
+                            alt='...'
+                        >
+                            <h5>Second slide label</h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </MDBCarouselItem>
 
-                    <MDBCarouselItem
-                        className='w-100 d-block'
-                        itemId={3}
-                        src='https://www.bankasia-bd.com/agentassets/slider/flash-3.jpg'
-                        alt='...'
-                    >
-                        <h5>Third slide label</h5>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </MDBCarouselItem>
+                        <MDBCarouselItem
+                            className='w-100 d-block'
+                            itemId={3}
+                            src='https://www.bankasia-bd.com/agentassets/slider/flash-3.jpg'
+                            alt='...'
+                        >
+                            <h5>Third slide label</h5>
+                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                        </MDBCarouselItem>
 
-                
-                </MDBCarousel>
+                    
+                    </MDBCarousel>
                 </div>
 
 
